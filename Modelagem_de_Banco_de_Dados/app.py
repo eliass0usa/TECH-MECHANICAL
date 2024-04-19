966f623cc9bdfd2c264e8a392f9a1d4a4bebc0ed
Elias Oliveira <eliassousaoliveira@hotmail.com>
2024-04-19 01:33:44 -0300

    Remodelagem dos dados

--- Modelagem_de_Banco_de_Dados/app.py
+++ Modelagem_de_Banco_de_Dados/app.py
@@ -1,42 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for
-from connection import criar_usuario, ler_usuarios, atualizar_senha, deletar_usuario
+from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path = '/static')
+app = Flask(__name__)
 
-# ROTA PARA A PAGINA INICIAL
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# CADASTRO DE USUÁRIO
-@app.route('/cadastrar', methods=['POST'])
-def cadastrar():
-    nome = request.form['nome']
-    email = request.form['email']
-    senha = request.form['senha']
-    criar_usuario(nome, email, senha)
-    return redirect(url_for('index'))
-
-# LISTAR USUÁRIOS
-@app.route('/usuarios')
-def listar_usuarios():
-    usuarios = ler_usuarios()
-    return render_template('usuarios.html', usuarios=usuarios)
-
-# LIDAR COM ATUALIZAÇÃO DE SENHA
-@app.route('/atualizar_senha', methods=['POST'])
-def atualizar_senha_usuario():
-    email = request.form['email']
-    nova_senha = request.form['nova_senha']
-    atualizar_senha(email, nova_senha)
-    return redirect(url_for('listar_usuarios'))
-
-# EXCLUSÃO DE USUARIO
-@app.route('/deletar_usuario', methods=['POST'])
-def deletar_usuario_route():
-    email = request.form['email']
-    deletar_usuario(email)
-    return redirect(url_for('listar_usuarios'))
+@app.route('/login')
+def login():
+    # Aqui você pode adicionar a lógica de login
+    return render_template('login.html')  # Renderiza o template de login
 
 if __name__ == '__main__':
     app.run(debug=True)
